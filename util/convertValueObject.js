0c2ba092e64d798f5a89e222f230c22fc15bd029
Dallas Howard <dallas@galacticwidgets.com>
2024-03-06 14:36:14 -0500

    convert stored dates to Date; added $currentDate

--- util/convertValueObject.js
+++ util/convertValueObject.js
@@ -1,25 +1,65 @@
-export const convertValueObject = (searchObject) => {
-    if(typeof searchObject == 'object'){
-        Object.keys(searchObject).forEach(key => {
+export const convertValueObject = (originalObject) => {
+    if(typeof originalObject == 'object'){
+        Object.keys(originalObject).forEach(key => {
             switch(key){
                 case '$multiply':                            
-                    searchObject = convertValueObject(searchObject[key][0]) * convertValueObject(searchObject[key][1]);
+                    originalObject = convertValueObject(originalObject[key][0]) * convertValueObject(originalObject[key][1]);
                     break;
                 case '$floor':
-                    searchObject = Math.floor(convertValueObject(searchObject[key]));
+                    originalObject = Math.floor(convertValueObject(originalObject[key]));
                     break;
                 case '$rand':                        
-                    searchObject = Math.random();
+                    originalObject = Math.random();
+                    break;
+                case '$currentDate':
+                    if(
+                        typeof originalObject[key] == 'object' &&
+                        Object.hasOwn(originalObject[key], '$type') && 
+                        (originalObject[key].$type == 'timestamp' || originalObject[key].$type == 'date')
+                    ){
+                        switch(originalObject[key].$type){
+                            case 'date':
+                                originalObject = new Date();
+                                break;
+                            case 'timestamp':
+                                originalObject = Date.now();
+                                break;
+                        }
+                    } else if(typeof originalObject[key] == 'boolean' && originalObject[key]){
+                        originalObject = new Date();
+                    } else {
+                        throw new Error(`convertValueObject error: $currentDate invalid format`);
+                    }
                     break;
                 default:
-                    if(typeof searchObject[key] == 'object' || Array.isArray(searchObject[key])) searchObject[key] = convertValueObject(searchObject[key]);
+                    if(typeof originalObject[key] == 'object' || Array.isArray(originalObject[key])) originalObject[key] = convertValueObject(originalObject[key]);
                     break;
             }
         });
-    } else if (Array.isArray(searchObject)){
-        searchObject.forEach(key => {
-            if(typeof searchObject[key] == 'object' || Array.isArray(searchObject[key])) searchObject[key] = convertValueObject(searchObject[key]);            
+    } else if (Array.isArray(originalObject)){
+        originalObject.forEach(key => {
+            if(typeof originalObject[key] == 'object' || Array.isArray(originalObject[key])) originalObject[key] = convertValueObject(originalObject[key]);            
         });
     }            
-    return searchObject;
+    return originalObject;
+}
+
+export const convertStoredDates = (originalObject) => {
+    if(typeof originalObject == 'object' && !(originalObject instanceof Date) && originalObject != null){
+        Object.keys(originalObject).forEach(key => {
+            if(typeof originalObject[key] == 'string' && originalObject[key].includes(':') && originalObject[key].includes('Z')){
+                try {
+                    originalObject[key] = new Date(originalObject[key])
+                } catch(error){
+                    // do nothing.
+                }
+            }
+            if((typeof originalObject[key] == 'object' || Array.isArray(originalObject[key])) && !(originalObject[key] instanceof Date)) originalObject[key] = convertStoredDates(originalObject[key]);
+        });
+    } else if (Array.isArray(originalObject)){
+        originalObject.forEach(key => {
+            if((typeof originalObject[key] == 'object' || Array.isArray(originalObject[key])) && !(originalObject[key] instanceof Date)) originalObject[key] = convertStoredDates(originalObject[key]);            
+        });
+    }            
+    return originalObject;
 }
